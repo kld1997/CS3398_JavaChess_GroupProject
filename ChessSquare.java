760c3046f976e50cc39233318f7f3f97f7171509
kth43 <54726840+kth43@users.noreply.github.com>
2019-09-26 17:20:30 -0500

    Update ChessSquare.java

--- ChessSquare.java
+++ ChessSquare.java
@@ -1,19 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseListener;
 
 public class ChessSquare extends JButton
 {
-    public ChessSquare(int color)
+    public ChessSquare(int color, int xCoord, int yCoord)
     {
         //setBorder(BorderFactory.createLineBorder(Color.RED, 2));
         if(color == 0)
         {
             setBackground(Color.BLACK);
         }
+        else
+        {
+            setBackground(Color.WHITE);
+        }
         setVisible(true);
     }
     public void Highlight()
     {
-        setBorder(BorderFactory.createLineBorder(Color.YELLOW, 3));
+        setBorder(BorderFactory.createLineBorder(Color.RED, 5));
     }
+
 }
