996a209a5150922d8fb25c269984c7880677d4b5
rag189 <54725814+rag189@users.noreply.github.com>
2019-09-26 18:33:30 -0500

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 public interface Piece
 {
-    public void movePiece(Board board, int x, int y);
+    public long possibleMoves(Board board, int x, int y);
+    public void movePiece(Board board, int x1, int y1, int x2, int y2);
 }
