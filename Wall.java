344c0f310eb0a2d655dd6ac1014555c53dca3e47
kth43 <54726840+kth43@users.noreply.github.com>
2019-10-29 16:40:46 -0500

    Create Wall.java

---
+++ Wall.java
@@ -0,0 +1,16 @@
+public class Wall extends Pieces
+{
+    public Wall(int t)
+    {
+        super(t);
+        value = 0;
+    }
+    /*
+    public long pieceType(NewBoard board)
+    {
+        return board.wallBB[team];
+    }
+    public long pseudoMoves(long occ, long coord){return 0;}
+
+     */
+}
